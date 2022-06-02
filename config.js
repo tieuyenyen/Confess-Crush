52a3ee0f75faa4614f5cab918f630e4f6cdc44d5
tieuyenyen <106748690+tieuyenyen@users.noreply.github.com>
2022-06-02 19:37:18 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "HI BẠN",
+    introTitle: 'BẠN DUẨN CÓ BỜ MÔI QUYẾN RỦ À',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
